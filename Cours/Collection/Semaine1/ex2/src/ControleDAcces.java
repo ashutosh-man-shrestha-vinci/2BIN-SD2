56661006f996eff9547c7d39b0943c8474398d6c
Ashutosh Man Shrestha <ashutosh.manshrestha@student.vinci.be>
2026-01-22 18:46:26 +0100

    new ex2

--- Cours/Collection/Semaine1/ex2/src/ControleDAcces.java
+++ Cours/Collection/Semaine1/ex2/src/ControleDAcces.java
@@ -1,25 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class ControleDAcces {
-	
-	public ControleDAcces(){
-		
+    HashMap<Badge, Employe> acces;
+    HashSet<Employe> employeHashMap;
+    public ControleDAcces(){
+        acces = new HashMap();
+        employeHashMap = new HashSet();
 	}
-	
 	// associe le badge � un employ�
 	public void donnerBadge (Badge b, Employe e){
+         acces.put(b, e);
 	}
 	
 	// met � jour les employ�s pr�sents dans le batiment
 	public void entrerBatiment (Badge b){
+        Employe e = acces.get(b);
+        employeHashMap.add(e);
 	}
 
 	// met � jour les employ�s pr�sents dans le batiment
 	public void sortirBatiment (Badge b){
+        Employe e = acces.get(b);
+
+        employeHashMap.remove(e);
 
 	}
 	
 	// renvoie vrai si l'employ� est dans le batiment, faux sinon
 	public boolean estDansBatiment (Employe e){
-		return false;
+
+		return employeHashMap.contains(e);
 	}
 
 }
