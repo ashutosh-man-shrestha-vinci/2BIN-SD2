0cdb4b6fb3c950c35c7574d137fe2093f6b8bbbe
Ashutosh <Ashutosh.manshrestha@student.vinci.be>
2026-02-09 11:52:34 +0100

    new ex 4

--- Cours/Collection/Semaine1/ex4/src/ChoixOptions.java
+++ Cours/Collection/Semaine1/ex4/src/ChoixOptions.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
 
 public class ChoixOptions {
 	
 	// associe le nom d'une option avec son objet Option correspondant
 	private Map<String, Option> options;
-	private HashMap<Etudiant,ArrayList<String>> choix;
-	private PriorityQueue<Etudiant> queue;
+	private SortedMap<Etudiant, String[]> etudiantChoix;
+
+	
 	//constructeur prenant un entier et une suite de string en param�tres
 	//ces string repr�sentent les noms des diff�rentes options possibles
 	public ChoixOptions(int nbEtudiantsParOption, String... nomsOption) {
@@ -20,21 +24,18 @@ public class ChoixOptions {
 			String nomOption = nomsOption[i];
 			options.put(nomOption, new Option(nomOption, nbEtudiantsParOption));
 		}
-		choix = new HashMap<>();
-		queue = new PriorityQueue<>((e1,e2) -> Integer.compare(e2.getMoyenne(), e1.getMoyenne()));
+		etudiantChoix = new TreeMap<>(Comparator.comparing(Etudiant::getMoyenne).reversed());
+		// initialiser les nouveaux attributs
 	}
 
 	// cette m�thode encode les pr�f�rences des �tudiants
 	// il ne faut pas v�rifier que ces choix soient valides
 	public void ajouterPreferences(Etudiant etu, String choix1, String choix2,
 			String choix3) {
-		if(!choix.containsKey(etu)){
-			choix.put(etu,new ArrayList<>());
-			queue.add(etu);
-		}
-    choix.get(etu).add(choix1);
-		choix.get(etu).add(choix2);
-		choix.get(etu).add(choix3);
+
+       String[] choixes = {choix1, choix2, choix3};
+      etudiantChoix.put(etu,choixes);
+
 	}
 
 	// cette m�thode est appel�e apr�s que les �tudiants aient donn� leurs pr�f�rences
@@ -42,15 +43,15 @@ public class ChoixOptions {
 	// ayant les meilleures moyennes si il n'y a plus de place disponible dans certaines 
 	// options. Pour les �tudiants faibles, si les deux premi�res options sont pleines, 
 	// il faut recourir au troisi�me choix.
-	// Cette m�thode doit faire appel é la m�thode inscrireEtudiant de la classe Option.
+	// Cette m�thode doit faire appel � la m�thode inscrireEtudiant de la classe Option.
 	public void attribuerOptions() {
-		while(!queue.isEmpty()) {
-			Etudiant etu = queue.poll();
-			for (String s : choix.get(etu)) {
-				  Option option = options.get(s);
-					if(option.inscrireEtudiant(etu)){
-						break;
-					}
+		for (Entry<Etudiant, String[]> etudiantEntry : etudiantChoix.entrySet()) {
+			    Etudiant etu = etudiantEntry.getKey();
+			for (String s : etudiantEntry.getValue()) {
+				   Option option = options.get(s);
+					 if(option.inscrireEtudiant(etu)){
+						 break;
+					 }
 			}
 		}
 	}
